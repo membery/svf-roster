99a6fd03fed76ae60f3660ecf923cda229be7b56
petugez <petugez@gmail.com>
2014-09-09 20:11:30 +0200

    Clean up codeQ.

--- src/server/RosterController.js
+++ src/server/RosterController.js
@@ -1,6 +1,6 @@
+'use strict';
 var log = require('./logging.js').getLogger('RosterController.js');
 var renderModule = require('./renderService.js');
-var auditLog = require('./logging.js').getLogger('AUDIT');
 var async = require('async');
 var QueryFilter = require('./QueryFilter.js');
 var hash = require('object-hash');
@@ -14,42 +14,41 @@ var RosterController = function(mongoDriver) {
 	
 	var rosterDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "rosters"}
+			{collectionName: 'rosters'}
 	);
 
 	var seasonDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "seasons"}
+			{collectionName: 'seasons'}
 	);
 
 
 	var peopleDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "people"}
+			{collectionName: 'people'}
 	);
 
 	var competitionDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "competitions"}
+			{collectionName: 'competitions'}
 	);
 
 	var clubDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "organizations"}
+			{collectionName: 'organizations'}
 	);
 
 	var transferDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "transfers"}
+			{collectionName: 'transfers'}
 	);
 	
 	var ageCategoryDao = new universalDaoModule.UniversalDao(
 			mongoDriver,
-			{collectionName: "ageCategories"}
+			{collectionName: 'ageCategories'}
 	);
 
 
-	var that=this;
 	this.mongoDriver=mongoDriver;
 
 	this.search=function (req,res){
@@ -67,9 +66,9 @@ var RosterController = function(mongoDriver) {
 		var qf=QueryFilter.create();
 			qf.addCriterium('club','ex',null);
 
-		toCall.push(function(callback){ seasonDao.list({},function(err,data){ if (err) {callback(err);return } seasons=data;callback(); });} );
-		toCall.push(function(callback){ competitionDao.list({},function(err,data){ if (err) {callback(err);return } competitions=data;callback(); });} );
-		toCall.push(function(callback){ clubDao.list(qf,function(err,data){ if (err) {callback(err);return } clubs=data;callback(); });} );	
+		toCall.push(function(callback){ seasonDao.list({},function(err,data){ if (err) {callback(err);return; } seasons=data;callback(); });} );
+		toCall.push(function(callback){ competitionDao.list({},function(err,data){ if (err) {callback(err);return; } competitions=data;callback(); });} );
+		toCall.push(function(callback){ clubDao.list(qf,function(err,data){ if (err) {callback(err);return; } clubs=data;callback(); });} );	
 
 		if (clubId || seasonId || compId)	{
 			var rosterqf=QueryFilter.create();
@@ -83,16 +82,21 @@ var RosterController = function(mongoDriver) {
 			rosterqf.addCriterium('baseData.competition.oid','eq',compId);
 			}
 			
-			toCall.push(function(callback){ rosterDao.list(rosterqf,function(err,data){ if (err) {callback(err);return } rosters=data;callback(); });} );	
+			toCall.push(function(callback){ rosterDao.list(rosterqf,function(err,data){ if (err) {callback(err);return; } rosters=data;callback(); });} );	
 		}
 		
-		async.parallel(toCall, function (err,callback){
+		async.parallel(toCall, function (err){
+			if (err){
+				log.(err);
+				res.send(500);
+				return;
+			}
 			var page=renderService.render(renderModule.templates.SEARCH,{seasons:seasons,competitions:competitions,clubs:clubs,rosters:rosters,selectedClub:clubId,selectedSeason:seasonId,selectedComp:compId});
 			res.send(200,page);
 		} );
 
 		
-	}
+	};
 
 	
 	this.roster=function (req,res){
@@ -100,39 +104,39 @@ var RosterController = function(mongoDriver) {
 		
 
 		rosterDao.get(rosterId,function(err,data){
-				if (err){resp.send(500); return;}
+				if (err){res.send(500); return;}
 		
 				var roster={prName:data.baseData.prName};
 				
 				var toCall=[];
-				toCall.push(function(callback){ seasonDao.get(data.baseData.season.oid,function(err,data){ if (err) {callback(err);return } roster.season=data.baseData.name;callback(); });} );
-				toCall.push(function(callback){ clubDao.get(data.baseData.club.oid,function(err,data){ if (err) {callback(err);return } roster.club=data.club.name;callback(); });} );
-				toCall.push(function(callback){ seasonDao.get(data.baseData.season.oid,function(err,data){ if (err) {callback(err);return } roster.season=data.baseData.name;callback(); });} );
-				toCall.push(function(callback){ competitionDao.get(data.baseData.competition.oid,function(err,data){ if (err) {callback(err);return } roster.competition=data.baseData.name;callback(); });} );
+				toCall.push(function(callback){ seasonDao.get(data.baseData.season.oid,function(err,data){ if (err) {callback(err);return; } roster.season=data.baseData.name;callback(); });} );
+				toCall.push(function(callback){ clubDao.get(data.baseData.club.oid,function(err,data){ if (err) {callback(err);return;} roster.club=data.club.name;callback(); });} );
+				toCall.push(function(callback){ seasonDao.get(data.baseData.season.oid,function(err,data){ if (err) {callback(err);return;} roster.season=data.baseData.name;callback(); });} );
+				toCall.push(function(callback){ competitionDao.get(data.baseData.competition.oid,function(err,data){ if (err) {callback(err);return;} roster.competition=data.baseData.name;callback(); });} );
 
-				toCall.push(function(callback){ peopleDao.get(data.coaches.coach.oid,function(err,data){ if (err) {callback(err);return } roster.coach={}; roster.coach.name=data.baseData.surName + ' '+ data.baseData.name; roster.coach.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+				toCall.push(function(callback){ peopleDao.get(data.coaches.coach.oid,function(err,data){ if (err) {callback(err);return;} roster.coach={}; roster.coach.name=data.baseData.surName + ' '+ data.baseData.name; roster.coach.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
 
-				toCall.push(function(callback){ ageCategoryDao.get(data.baseData.ageCategory.oid,function(err,data){ if (err) {callback(err);return } roster.category=data.baseData.name;;callback(); });} );
+				toCall.push(function(callback){ ageCategoryDao.get(data.baseData.ageCategory.oid,function(err,data){ if (err) {callback(err);return;} roster.category=data.baseData.name;callback(); });} );
 
 				
 				if (data.coaches.aCoach1.oid){
-					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach1.oid,function(err,data){ if (err) {callback(err);return }  roster.assistant1={}; roster.assistant1.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant1.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ')  ;callback(); });} );
+					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach1.oid,function(err,data){ if (err) {callback(err);return;}  roster.assistant1={}; roster.assistant1.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant1.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ')  ;callback(); });} );
 				}
 
 				if (data.coaches.aCoach2.oid){
-					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach2.oid,function(err,data){ if (err) {callback(err);return } roster.assistant2={};roster.assistant2.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant2.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach2.oid,function(err,data){ if (err) {callback(err);return;} roster.assistant2={};roster.assistant2.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant2.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
 				}
 
 				if (data.coaches.aCoach3.oid){
-					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach3.oid,function(err,data){ if (err) {callback(err);return } roster.assistant3={};roster.assistant3.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant3.license=((data.coach && data.coach.coachLicense) || '') +' / '+ ((data.coach && data.coach.coachLicenseType) || '') ;callback(); });} );
+					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach3.oid,function(err,data){ if (err) {callback(err);return;} roster.assistant3={};roster.assistant3.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant3.license=((data.coach && data.coach.coachLicense) || '') +' / '+ ((data.coach && data.coach.coachLicenseType) || '') ;callback(); });} );
 				}
 
 				if (data.coaches.aCoach4.oid){
-					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach4.oid,function(err,data){ if (err) {callback(err);return } roster.assistant4={};roster.assistant4.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant4.license=((data.coach && data.coach.coachLicense) || '') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach4.oid,function(err,data){ if (err) {callback(err);return;} roster.assistant4={};roster.assistant4.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant4.license=((data.coach && data.coach.coachLicense) || '') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
 				}
 
 				if (data.coaches.aCoach5.oid){
-					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach5.oid,function(err,data){ if (err) {callback(err);return } roster.assistant5={};roster.assistant5.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant5.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+					toCall.push(function(callback){ peopleDao.get(data.coaches.aCoach5.oid,function(err,data){ if (err) {callback(err);return;} roster.assistant5={};roster.assistant5.name=data.baseData.surName + ' '+ data.baseData.name;roster.assistant5.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
 				}
 
 				
@@ -143,7 +147,7 @@ var RosterController = function(mongoDriver) {
 				data.listOfPlayers.players.map(function(item){
 					item.id=index++;
 					toCall.push(function(callback){  peopleDao.get(item.oid,function(err,data){
-						if (err) {callback(err);return } 
+						if (err) {callback(err);return;} 
 						var player={surName:data.baseData.surName,name: data.baseData.name,birthDate: convertDate(data.baseData.birthDate), license: data.player.playerLicense}; 
 						players[item.id]= player;
 						var pqf = QueryFilter.create();
@@ -156,7 +160,6 @@ var RosterController = function(mongoDriver) {
 									callback(err);
 								}
 								
-								console.log('DATA', data);
 								if (data && data.length > 0) {
 								localPlayer.note = (data[0].baseData.typeOfTransfer || '') + ':' + (convertDate(data[0].baseData.dateTo) || '') ;
 								} else {
@@ -170,7 +173,7 @@ var RosterController = function(mongoDriver) {
 				});
 
 
-				async.parallel(toCall, function (err,callback){
+				async.parallel(toCall, function (err){
 						if (data.baseData.gender=='M'){
 							roster.gender='Muži';
 						}
@@ -178,20 +181,17 @@ var RosterController = function(mongoDriver) {
 							roster.gender='Ženy';
 						}
 						roster.players=players;
-
 						roster.lastModification=convertDate(data.baseData.lastModification);
-						
 						var page=renderService.render(renderModule.templates.ROSTER,{roster:roster,hash:hash.sha1(JSON.stringify(roster))});
 						res.send(200,page);
 					} );
 
-			
 		});
 
 
-	}
+	};
 
-}
+};
 
 function convertDate(date){
 	return date.substring(6)+'.'+date.substring(4,6)+'.'+date.substring(0,4) ;
@@ -199,4 +199,4 @@ function convertDate(date){
 
 module.exports = {
 	RosterController: RosterController
-}
+};
