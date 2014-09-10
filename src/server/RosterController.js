cf5e7b61180f9e7b11a0154d27485b86f275efc9
Pavol Starek <pavol@starek.sk>
2014-09-10 19:04:15 +0200

    FIX strange behavior when there is no coach set on roster
    - roster now displays correctly even there is no coach set

--- src/server/RosterController.js
+++ src/server/RosterController.js
@@ -114,7 +114,9 @@ var RosterController = function(mongoDriver) {
 				toCall.push(function(callback){ seasonDao.get(data.baseData.season.oid,function(err,data){ if (err) {callback(err);return;} roster.season=data.baseData.name;callback(); });} );
 				toCall.push(function(callback){ competitionDao.get(data.baseData.competition.oid,function(err,data){ if (err) {callback(err);return;} roster.competition=data.baseData.name;callback(); });} );
 
-				toCall.push(function(callback){ peopleDao.get(data.coaches.coach.oid,function(err,data){ if (err) {callback(err);return;} roster.coach={}; roster.coach.name=data.baseData.surName + ' '+ data.baseData.name; roster.coach.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+				if (data.coaches.aCoach1.oid) {
+					toCall.push(function(callback){ peopleDao.get(data.coaches.coach.oid,function(err,data){ if (err) {callback(err);return;} roster.coach={}; roster.coach.name=data.baseData.surName + ' '+ data.baseData.name; roster.coach.license=((data.coach && data.coach.coachLicense) || ' ') +' / '+ ((data.coach && data.coach.coachLicenseType) || ' ') ;callback(); });} );
+				}
 
 				toCall.push(function(callback){ ageCategoryDao.get(data.baseData.ageCategory.oid,function(err,data){ if (err) {callback(err);return;} roster.category=data.baseData.name;callback(); });} );
 
